1a5c2d443227b84fc021014149c79c933aa93728
William Meighan <williamdmeighan@gmail.com>
2019-11-19 19:00:20 -0700

    Add initialize, current card, take turn, and number correct methods and passing tests

---
+++ lib/round.rb
@@ -0,0 +1,27 @@
+class Round
+
+  attr_reader :deck, :turns
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+  end
+
+  def current_card
+    deck.cards.first
+  end
+
+  def take_turn(guess)
+    @turns << Turn.new(guess, current_card)
+    @deck.cards.shift
+    @turns.last
+  end
+
+  def number_correct
+    @turns.find_all do |turn|
+      turn.correct?
+    end.length
+  end
+
+
+end
